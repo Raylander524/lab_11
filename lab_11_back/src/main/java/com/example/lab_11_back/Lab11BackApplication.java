eee3d685a7c3991923d2a70562980276e3e38076
Raylander524 <raylandermarques24@gmail.com>
2022-07-04 22:07:30 -0300

    Começando a implementação

---
+++ lab_11_back/src/main/java/com/example/lab_11_back/Lab11BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_11_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab11BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab11BackApplication.class, args);
+	}
+
+}
